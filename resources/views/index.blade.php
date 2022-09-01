ab20f0e69c6652788deb9de52cb8a3084f8bfe72
Thế Anh <bestbacsivbb@gmail.com>
2022-09-01 15:39:19 +0700

    fix lai giao dien

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,84 +5,51 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Nền tảng - Kiến thức cơ bản về WEB | Bảng tin</title>
-
+    <title>Tiny Shop| Chuyên đồ điện tử</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ url('./vendor/bootstrap/css/bootstrap.min.css') }}" type="text/css">
     <!-- Font awesome -->
     <link rel="stylesheet" href="{{ url('./vendor/font-awesome/css/font-awesome.min.css') }}" type="text/css">
-<<<<<<< HEAD
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
-=======
-
->>>>>>> 0181047a971f83579d44536327046c1e4d523cc3
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+          integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <!-- Custom css - Các file css do chúng ta tự viết -->
     <link rel="stylesheet" href="{{ url('./css/app.css') }}" type="text/css">
 </head>
 
 <body>
-    <!-- header -->
-<<<<<<< HEAD
-    <nav class="navbar navbar-expand-custom navbar-mainbg bg-dark">
-        <a class="navbar-brand navbar-logo" href="#">Trang chủ</a>
-        <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<!-- header -->
+<nav class="navbar navbar-expand-custom navbar-mainbg bg-dark">
+    <a class="navbar-brand navbar-logo" href="#">Trang chủ</a>
+    <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
         <i class="fas fa-bars text-white"></i>
-        </button>
-        <form class="form-inline mt-2 mt-md-0" method="get" action="pages/search.html">
-                    <input class="form-control mx-0 rounded-left" type="text" placeholder="Tìm kiếm" aria-label="Search"
-                        name="keyword_tensanpham">
-                    <button class="btn btn-success my-2 my-sm-0 mx-0 rounded-right" type="submit"><i class=" fas fa-thin fa-magnifying-glass"></i></button>
-        </form>
-=======
-    <nav class="navbar navbar-expand-custom navbar-mainbg">
-        <a class="navbar-brand navbar-logo" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fas fa-bars text-white"></i>
-        </button>
->>>>>>> 0181047a971f83579d44536327046c1e4d523cc3
+    </button>
+    <form class="form-inline mt-2 mt-md-0" method="get" action="pages/search.html">
+        <input class="form-control mx-0 rounded-left" type="text" placeholder="Tìm kiếm" aria-label="Search"
+               name="keyword_tensanpham">
+        <button class="btn btn-success my-2 my-sm-0 mx-0 rounded-right" type="submit"><i
+                class=" fas fa-thin fa-magnifying-glass"></i></button>
+    </form>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
-                <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
+                <div class="hori-selector">
+                    <div class="left"></div>
+                    <div class="right"></div>
+                </div>
                 <li class="nav-item">
-<<<<<<< HEAD
                     <a class="nav-link" href="javascript:void(0);"><i class="fas fa-tachometer-alt"></i>Sản phẩm</a>
                 </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-address-book"></i>Liên hệ</a>
-=======
-                    <a class="nav-link" href="javascript:void(0);"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-address-book"></i>Address Book</a>
-                </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-clone"></i>Components</a>
->>>>>>> 0181047a971f83579d44536327046c1e4d523cc3
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-calendar-alt"></i>Calendar</a>
-                </li>
-                <li class="nav-item">
-<<<<<<< HEAD
                     <a class="nav-link" href="javascript:void(0);"><i class="far fa-chart-bar"></i>Giỏ hàng</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="javascript:void(0);"><i class="far fa-copy"></i>Đăng nhập</a>
-=======
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-chart-bar"></i>Charts</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-copy"></i>Documents</a>
->>>>>>> 0181047a971f83579d44536327046c1e4d523cc3
-                </li>
+               </li>
             </ul>
         </div>
     </nav>
     <!-- end header -->
-
-
-
     <main role="main">
         <!-- Carousel - Slider -->
         <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -133,19 +100,19 @@
             <div class="row">
                 <div class="col-lg-4">
                     <img class="bd-placeholder-img rounded-circle" width="140" height="140"
-                        src="{{ url('./img/icon/icon-1.png') }}">
+                         src="{{ url('./img/icon/icon-1.png') }}">
                     <h2>Đặt hàng</h2>
                     <p>Chọn sản phẩm bạn yêu thích, và Đặt hàng.</p>
                 </div><!-- /.col-lg-4 -->
                 <div class="col-lg-4">
                     <img class="bd-placeholder-img rounded-circle" width="140" height="140"
-                        src="{{ url('./img/icon/icon-2.png') }}">
+                         src="{{ url('./img/icon/icon-2.png') }}">
                     <h2>Tạo đơn hàng</h2>
                     <p>Theo dõi đơn hàng của bạn.</p>
                 </div><!-- /.col-lg-4 -->
                 <div class="col-lg-4">
                     <img class="bd-placeholder-img rounded-circle" width="140" height="140"
-                        src="{{ url('./img/icon/icon-3.png') }}">
+                         src="{{ url('./img/icon/icon-3.png') }}">
                     <h2>Giao hàng</h2>
                     <p>Giao hàng tận nơi.</p>
                 </div><!-- /.col-lg-4 -->
@@ -201,7 +168,7 @@
                         <div class="card mb-4 shadow-sm">
                             <a href="pages/product-detail.html">
                                 <img class="bd-placeholder-img card-img-top" width="100%" height="350"
-                                    src="{{ url('./img/product/samsung-s3.webp') }}">
+                                     src="{{ url('./img/product/samsung-s3.webp') }}">
                             </a>
                             <div class="card-body">
                                 <a href="pages/product-detail.html">
@@ -212,7 +179,7 @@
                                 <div class="d-flex justify-content-between align-items-center">
                                     <div class="btn-group">
                                         <a class="btn btn-sm btn-outline-secondary"
-                                            href="pages/product-detail.html">Xem chi tiết</a>
+                                           href="pages/product-detail.html">Xem chi tiết</a>
                                     </div>
                                     <small class="text-muted text-right">
                                         <s>12,600,000.00</s>
@@ -223,7 +190,7 @@
                         </div>
                     </div>
 
-                </div>   
+                </div>
             </div>
         </div>
 
@@ -250,14 +217,10 @@
     <script src="{{ url('./vendor/bootstrap/js/bootstrap.min.js') }}"></script>
 
     <!-- Custom script - Các file js do mình tự viết -->
-<<<<<<< HEAD
     <script src="{{ url('./js/app.js') }}"></script>
-    
-=======
     <!-- Js -->
     <script src="{{ url('./js/app.js') }}"></script>
 
->>>>>>> 0181047a971f83579d44536327046c1e4d523cc3
 </body>
 
 </html>
